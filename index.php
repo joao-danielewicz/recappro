a047e30a5ac041023a6dd64e94596fa53ebbfcbb
João Danielewicz <joao.wicz@gmail.com>
2024-11-28 19:20:15 -0300

    commit

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 require_once __DIR__.'/Core/Core.php';
 require_once __DIR__.'/Router/routes.php';
 
+// O arquivo .htaccess é usado para definir algumas configurações do servidor local. Nele, incluímos algumas regras que irão redirecionar o usuário para a página index caso a URL digitada não corresponda a um arquivo ou pasta acessível na árvore do servidor. 
+// Além disso, há uma tratativa para que seja possível capturar tudo que seja passado após localhost:8000, o que nos faz capaz de encaminhar o usuário dinamicamente.
 
+// Usamos esta função para carregar automaticamente todas as classes que iremos usar em nossa aplicação, como os Models, Controllers, Utils, etc.
 spl_autoload_register(function($file){
     if(file_exists(__DIR__."/utils/$file.php")){
         require_once __DIR__."/utils/$file.php";
